6e1822b588bb550f0555f95eecfe289700e2b76e
btlmm410 <37677650+btlmm410@users.noreply.github.com>
2018-09-24 22:09:28 +0800

    Update main.py

--- hw03/main.py
+++ hw03/main.py
@@ -1,8 +1,6 @@
-for i in range(9,0,-1):
-    for g in range(0,9-i):
-	    print(end="                 ")
-    for j in range( i,0,-1):
-        a='{0}x{1}={2}'.format(i,j,i*j)
-        print('%10s'%a,end=' ')
-
-    print()
+for i in range(1,10):
+    for k in range(1,i):
+        print(end="       ")
+    for j in range(i,10):
+        print('{0}*{1}={2}'.format(i,j,i*j),end=' ')
+    print( )
